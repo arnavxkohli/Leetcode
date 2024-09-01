c8f09dc6050ae443c759b6a757d6d60ebe2a3f0f
Arnav Kohli <arnavkohli2003@gmail.com>
2024-09-01 17:21:45 +0100

    Added CPP solution for 1514
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- path-with-maximum-probability/solution.cpp
+++ path-with-maximum-probability/solution.cpp
@@ -1,18 +1,17 @@
+#include <functional>
 #include <queue>
 #include <vector>
 using namespace std;
-
-struct Comparator {
-  bool operator()(pair<double, int> _first, pair<double, int> _second) const {
-    return (_first.first < _second.first);  // Max heap based on probability
-  }
-};
 
 class Solution {
 public:
   double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
     vector<double> distances(n, 0.0);
     distances[start_node] = 1.0;
+
+    auto cmp = [](pair<int, double> l, pair<int, double> r) {
+      return l.first < r.first;
+    };
 
     vector<vector<pair<int, double>>> graph(n);
     for(int i=0; i<edges.size(); i++) {
@@ -21,7 +20,8 @@ public:
       graph[w].push_back({u, d});
     }
 
-    priority_queue<pair<double, int>, vector<pair<double, int>>, Comparator> pq;
+    priority_queue<pair<double, int>, vector<pair<double, int>>, decltype(cmp)> pq(cmp);
+
     pq.push({1.0, start_node});
     while(pq.size() > 0) {
       auto [cur_dist, cur_node] = pq.top();
